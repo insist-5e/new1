a55d27744c435c85dcf2a93b4efc9f40e05f73c5
insist-5e <liusiyu132720@icloud.com>
2020-04-09 16:27:52 +0800

    Update new11.cpp

---
+++ new11.cpp
@@ -0,0 +1,25 @@
+//mode by lsy
+#include<bits/stdc++.h>
+#define ll long long
+#define pq priority_queue
+#define repr(i,x,y) for(int i=x;i<=y;i++)
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define drep(i,x,y) for(int i=x;i>=y;i--)
+#define pb push_back
+#define P make_pair
+#define pii pair<int,int>
+#define mem(x,y) memset(x,y,sizeof(x));
+using namespace std;
+const int inf = 0x3f3f3f3f, maxn = 2e5 + 5;
+const double eps = 1e-8;
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //cout<<endl<<clock();
+    return 0;
+}
+/*
+    Simplicity is a prerequisite for stability.
+            ——by acmer lsy
+*/
